aaacb848959d426918f74708fba036b0f2e584d7
Stefan_Yang <923775370@qq.com>
2019-12-27 13:55:08 +0800

    录制屏幕

--- RecordVideo/js/app.js
+++ RecordVideo/js/app.js
@@ -14,10 +14,11 @@ new Vue({
     methods: {
 
         async _initApp() {
-            this._stream = await navigator.mediaDevices.getUserMedia({audio: false, video: true});
+            // this._stream = await navigator.mediaDevices.getUserMedia({audio: false, video: true});
+            this._stream = await navigator.mediaDevices.getDisplayMedia();  //录制屏幕
             this.$refs.preview.srcObject = this._stream;
             this._recorder = new MediaRecorder(this._stream, {mimeType: "video/webm;codecs=h264"});
-            this._recorder.ondataavailable = this.recorder_dataAvailableHandler.bind(this);
+            this._recorder.ondataavailable = this.recorder_dataAvailableHandler.bind(this);    //监听，当有数据时执行
         },
 
         recorder_dataAvailableHandler(e) {
